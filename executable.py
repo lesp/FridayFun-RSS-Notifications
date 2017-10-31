6f0ac4b5affa705103afcbdf92146a80936afc19
Les Pounder <lespounder@gmail.com>
2017-10-31 18:48:59 +0000

    Added code, yay

---
+++ executable.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import feedparser
+import notify2
+import time
+feed = feedparser.parse(sys.argv[1])
+notify2.init("Latest News from "+str(sys.argv[1]))
+
+for i in range(3):
+    info = feed["entries"][i]["title"]
+    URL = feed.entries[i]["link"]
+    icon = "/home/les/Documents/LXF/232/RSS-Notif/techradar.jpg"
+    n = notify2.Notification(info,URL,icon=icon)
+    n.show()
+    
+
