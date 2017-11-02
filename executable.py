4da578b757be9ad3cfd3ecd9c14afd4fadabde67
Les Pounder <lespounder@gmail.com>
2017-11-02 18:22:24 +0000

    updated

--- executable.py
+++ executable.py
@@ -8,7 +8,7 @@ notify2.init("Latest News from "+str(sys.argv[1]))
 for i in range(3):
     info = feed["entries"][i]["title"]
     URL = feed.entries[i]["link"]
-    icon = "/home/les/Documents/LXF/232/RSS-Notif/Pi.png"
+    icon = "/home/les/Documents/blog/RSS-Notif/Pi.png"
     n = notify2.Notification(info,URL,icon=icon)
     n.show()
     
