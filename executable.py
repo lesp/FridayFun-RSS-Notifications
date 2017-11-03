8e4a80b775d064fe5ca2c5626b15095aba8deb61
Les Pounder <lespounder@gmail.com>
2017-11-03 10:34:08 +0000

    Major changes as cron was a bust

--- executable.py
+++ executable.py
@@ -2,14 +2,17 @@
 import sys
 import feedparser
 import notify2
+import time
 feed = feedparser.parse(sys.argv[1])
+delay = int(sys.argv[2])
 notify2.init("Latest News from "+str(sys.argv[1]))
 
-for i in range(3):
-    info = feed["entries"][i]["title"]
-    URL = feed.entries[i]["link"]
-    icon = "/home/les/Documents/blog/RSS-Notif/Pi.png"
-    n = notify2.Notification(info,URL,icon=icon)
-    n.show()
-    
+while True:
+    for i in range(3):
+        info = feed["entries"][i]["title"]
+        URL = feed.entries[i]["link"]
+        icon = "/home/les/Documents/blog/RSS-Notif/Pi.png"
+        n = notify2.Notification(info,URL,icon=icon)
+        n.show()
+    time.sleep(delay)
 
